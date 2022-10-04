58ee0e3b760c2cf35ef3c3231f7f801fd714311c
jinshinvn <thinhnguyen.280202@gmail.com>
2022-10-04 10:09:12 +0700

    ok

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
+const random = require('random');
 const { default: simpleGit } = require('simple-git');
-const simpleGit = require('simple-git');
+// const simpleGit = require('simple-git');
 
 const FILE_PATH = "./json/data.json";
 const DATE = moment().format();
 
-const data = {
-  date: DATE
+
+
+const makeCommit = n => {
+  if (n == 0)
+    return simpleGit().push();
+  const x = random.int(0, 54)
+  const y = random.int(0, 6)
+  const DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format()
+  const data = {
+    date: DATE
+  }
+  console.log(DATE)
+  jsonfile.writeFile(FILE_PATH, data, () => {
+    // git commit pass parameter day
+    simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE },
+    makeCommit.bind(this, --n))
+  })
 }
+  
+makeCommit(20);
 
-jsonfile.writeFile(FILE_PATH, data)
-
-// git commit pass parameter day
-simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE})
